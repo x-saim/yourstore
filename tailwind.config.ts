ba5096bae1533107893613144eff2ded69fade25
Saim Amir <saimxamir@gmail.com>
2025-03-17 01:12:59 -0400

    initial commit, project set up

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./assets/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+      }
+    },
+  },
+  plugins: [],
+}
