6a88e6d1ca32f0ffb62a956fbdff50e323f48f23
Saim Amir <saimxamir@gmail.com>
2025-03-18 01:39:35 -0400

    create root layout and constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
-
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YourStore",
-  description: "A modern ecommerce store built with Next.js.",
+  title: {
+    template: "%s | " + APP_NAME,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
