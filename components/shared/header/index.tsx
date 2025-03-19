941986da86df2cbb746abce75824fd2cbcf87ee5
Saim Amir <saimxamir@gmail.com>
2025-03-18 23:15:03 -0400

    add shadcn sheet component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,37 +1,27 @@
-import { ShoppingBag, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
-    <header className='w-full border-b'>
+    <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
-            <Image src="/images/logo.svg" alt={`${APP_NAME} logo`} width={48} height={48} priority={true} />
-            <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              width={48}
+              height={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          {/* Cart Button */}
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingBag className="w-5 h-5" />
-              <span className="hidden lg:block">Cart</span>
-            </Link>
-          </Button>
-          {/* Sign In Button */}
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon className="w-5 h-5" />
-              <span className="hidden lg:block">Sign In</span>
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
