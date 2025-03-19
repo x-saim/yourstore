b76eac034a8d25afbd13c5ed199c169fd587dd0a
Saim Amir <saimxamir@gmail.com>
2025-03-19 02:48:19 -0400

    create utility function to convert prisma obj to JS obj

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Convert a Prisma object to a plain JS object
+export function convertToPlainObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value));
+}
