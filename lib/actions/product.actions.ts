df9eefcc98cda4c1760c7ea722192d9e1702253a
Saim Amir <saimxamir@gmail.com>
2025-03-19 22:05:33 -0400

    extend prisma client with neon adaptor

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,10 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { prisma } from '@/db/prisma';
 
 // Get the latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
