56a4850f832e2bbe6049d04cbdd3acc4ac1f533e
Saim Amir <saimxamir@gmail.com>
2025-03-19 21:16:37 -0400

    add zod validation and product schema

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,11 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+// Format number with decimal places
+export function formatNumberWithDecimalPlaces(num: number): string {
+  // Split the number into integer and decimal parts
+  const [integer, decimal] = num.toString().split('.');
+  // Return the formatted number with two decimal places
+  return decimal ? `${integer}.${decimal.padEnd(2, '0')}` : `${integer}.00`;
+}
---
+++ lib/validator.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { formatNumberWithDecimalPlaces } from "./utils";
+
+const currency = z
+  .string()
+  .refine(
+    (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimalPlaces(Number(value))),
+    'Price must have exactly two decimal places (e.g., 49.99)'
+  );
+  
+// Schema for inserting a product
+export const insertProductSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  slug: z.string().min(3, 'Slug must be at least 3 characters'),
+  category: z.string().min(3, 'Category must be at least 3 characters'),
+  brand: z.string().min(3, 'Brand must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be at least 3 characters'),
+  stock: z.coerce.number(), // Convert to number
+  images: z.array(z.string()).min(1, 'Product must have at least one image'),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: currency,
+});
+
