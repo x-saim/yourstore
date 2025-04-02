553078f1aa6d0dce56c1972b80e52ff21aff86c9
Saim Amir <saimxamir@gmail.com>
2025-04-02 11:08:48 -0400

    next auth installation and config set up

---
+++ auth.ts
@@ -0,0 +1,74 @@
+import { compareSync } from 'bcrypt-ts-edge';
+import type { NextAuthConfig } from 'next-auth';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { prisma } from '@/db/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+export const config = {
+  // Pages
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+  },
+  // Session
+  session: {
+    strategy: 'jwt',
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  // Providers
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        // Find user in database
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!user || !user.password) {
+          // No user found or user doesn't have a password (e.g., OAuth user)
+          return null;
+        }
+
+        // Validate password
+        const isPasswordValid = compareSync(
+          credentials.password as string,
+          user.password
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        // Return user object if credentials are valid
+        // Exclude password from the returned user object
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }) {
+      session.user.id = token.sub as string;
+      if (trigger === 'update') {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
