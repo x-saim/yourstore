fa1f40b3f771a1975127488e77e78b6b21f9e74b
Saim Amir <saima@formulaconsulting.ca>
2025-04-03 07:51:27 -0400

    remove constant values

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -14,14 +14,14 @@ export const LATEST_PRODUCTS_LIMIT =
 
 // Sign in constants
 export const signInDefaultValues = {
-  email: 'john@doe.com',
-  password: 'password',
+  email: '',
+  password: '',
 };
 
 // Sign up constants
 export const signUpDefaultValues = {
-  name: 'Steve Smith',
-  email: 'steve@example.com',
-  password: 'password',
-  confirmPassword: 'password',
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
 };
